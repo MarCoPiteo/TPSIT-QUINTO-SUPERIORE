8f56534ea93abb70e1a32d65171386d27ad99a5b
MarCoPiteo <31marcopiteo31@gmail.com>
2024-03-13 11:29:22 +0100

    commit

--- ESERCIZI/PHP/Esercizio Dati Json Github/users.php
+++ ESERCIZI/PHP/Esercizio Dati Json Github/users.php
@@ -1,21 +1,19 @@
 <?php
-$users = [
-    ['id' => 1, 'name' => 'John Doe', 'hobby' => 'Recitazione'],
-    ['id' => 2, 'name' => 'Jane Doe', 'hobby' => 'Recitazione'],
-    ['id' => 3, 'name' => 'Alice', 'hobby' => 'Danza'],
-    ['id' => 4, 'name' => 'Bob', 'hobby' => 'Canto'],
-];
+    $users = [
+        ['id' => 1, 'name' => 'Marco', 'surname' => 'Ferri', 'age' => 18],
+        ['id' => 2, 'name' => 'Marco', 'surname' => 'Piteo', 'age' => 18],
+        ['id' => 3, 'name' => 'Cristian', 'surname' => 'Pesola', 'age' => 18],
+        ['id' => 4, 'name' => 'Damiano', 'surname' => 'Favale', 'age' => 18]
+    ];
 
-http_response_code(200);
-header('Content-Type: application/json');
-echo json_encode([
-    "status" => 200,
-    "message" => "Success",
-    "payload" => $users
-]);
+    http_response_code(200);
+    header('Content-Type: application/json');
 
-//echo json_encode($users);
-exit;
-
-
+    echo json_encode([
+        "status" => 200,
+        "message" => "Success",
+        "payload" => $users
+    ]); 
+    
+    exit;
 ?>
